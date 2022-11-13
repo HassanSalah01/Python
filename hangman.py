a539b77532b58149e688abc705175986832758b6
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-11-13 17:39:57 +0200

    Added_HangMan_Game

---
+++ hangman.py
@@ -0,0 +1,28 @@
+strArr = []
+solution = []
+word = "python"
+
+for i in word:
+    strArr.append(i)
+
+
+print(strArr)
+def isRight(word,wChar):
+    for i in word:
+        if(i==wChar):
+            solution.append(i)
+            break
+
+def lines(word,wChar):
+    str =""
+    for i in range(len(word)):
+        if(word[i]==wChar):
+            str+=word[i]
+        else:
+            str+="_"
+    print(str)
+
+
+lines("python","o")
+lines("python","p")
+
