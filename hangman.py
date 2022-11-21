b18c7407159e57152149056e076e828e3a39ba38
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2022-11-21 21:06:05 +0200

    Updated_hangman_Game

--- hangman.py
+++ hangman.py
@@ -13,26 +13,36 @@ def test2(wordCheker,wordAsChar):
     for i in range(len(wordCheker)):
         if(type(wordCheker[i])==str):
             newArray+="_"
-
         else:
             newArray+=wordAsChar[i]
     return newArray
+
 def test1(word,wordSlice,pInp,):
     for i in word :
         if(i==pInp):
             wordSlice.append(pInp)
-            break
+            print("tes")
+            return True
+    return False
 
 def main():
     word = "pythonjavascript"
     wordSlice = ["s"]
     wordAsChar=[c for c in word]
     wordCheker=wordAsChar[:]
-    test(wordSlice,word,wordCheker)
-    print(test2(wordCheker,wordAsChar))
-    test1(word,wordSlice,"a")
-    test(wordSlice,word,wordCheker)
-    print(test2(wordCheker,wordAsChar))
+    playerTrun = 3
+    while(playerTrun):
+        x = input()
+        if(test1(word,wordSlice,x)):
+            test(wordSlice,word,wordCheker)
+            print(test2(wordCheker,wordAsChar))
+        else:
+            playerTrun-=1
+            print("Wrong Input")
+
+    # test1(word,wordSlice,"a")
+    # test(wordSlice,word,wordCheker)
+    # print(test2(wordCheker,wordAsChar))
 
 
 
